700afa661a338432d00d528819920433553abc28
leb2793 <jufiar93@gmail.com>
2020-09-23 01:05:16 -0500

    v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MeetComponent } from './meet/meet.component';
+import { ClientesComponent } from './clientes/clientes.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: 'meet', component: MeetComponent},
+  {path: 'clientes', component: ClientesComponent},
+  {path:'**',redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './main-header/main-header.component';
+import { MenuHeaderComponent } from './menu-header/menu-header.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { WhatComponent } from './what/what.component';
+import { RecentWorkComponent } from './recent-work/recent-work.component';
+import { ServicesComponent } from './services/services.component';
+import { BackComponent } from './back/back.component';
+import { OurBlogComponent } from './our-blog/our-blog.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { MeetComponent } from './meet/meet.component';
+import { PiePaginaComponent } from './pie-pagina/pie-pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    MenuHeaderComponent,
+    WelcomeComponent,
+    WhatComponent,
+    RecentWorkComponent,
+    ServicesComponent,
+    BackComponent,
+    OurBlogComponent,
+    ClientesComponent,
+    MeetComponent,
+    PiePaginaComponent
   ],
   imports: [
     BrowserModule,
